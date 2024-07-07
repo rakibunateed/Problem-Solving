fa7ccdae0e1903806701901e8bd54e9e941556a7
rakibunateed <rakibunateed@gmail.com>
2024-07-08 01:11:21 +0600

    bear and brother

---
+++ A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdtr1c++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int count=0;
+    while (a<=b)
+    {
+        a*=3;
+        b*=2;
+        count++;
+      
+    }
+    cout<<count;
+    
+    return 0;
+}
